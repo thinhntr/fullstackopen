4f09b267f79f89ca1a4df70266430efb9459243b
thinhntr <thinhntr@gmail.com>
2022-08-12 11:47:30 +0700

    Ex 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,18 +4,33 @@ import Numbers from './components/Numbers';
 import ControlledInput from './components/ControlledInput';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
-  const [newPhone, setNewPhone] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (e) => {
     e.preventDefault();
+    // Check duplicate name
     if (persons.map((p) => p.name).includes(newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons((persons) => persons.concat({ name: newName }));
+
+    // Check empty phone number
+    if (newNumber.trim() === '') {
+      alert('Please enter a valid phone number');
+      return;
+    }
+
+    const newPerson = { name: newName, number: newNumber };
+    setPersons((persons) => persons.concat(newPerson));
     setNewName('');
+    setNewNumber('');
   };
 
   return (
@@ -29,8 +44,8 @@ const App = () => {
         />
         <ControlledInput
           text="number"
-          value={newPhone}
-          onChange={(e) => setNewPhone(e.target.value)}
+          value={newNumber}
+          onChange={(e) => setNewNumber(e.target.value)}
         />
         <div>
           <button type="submit">add</button>
--- part2/phonebook/src/components/Numbers.jsx
+++ part2/phonebook/src/components/Numbers.jsx
@@ -2,7 +2,7 @@ const Numbers = ({ persons }) => (
   <div>
     <h2>Numbers</h2>
     {persons.map((p) => (
-      <p key={p.name}>{p.name}</p>
+      <p key={p.name}>{p.name} {p.number}</p>
     ))}
   </div>
 );
