f000519be8de228c3324feb3db40e7d51563b3ea
thinhntr <thinhntr@gmail.com>
2022-08-13 01:46:24 +0700

    Ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Form from './components/Form';
 import Persons from './components/Persons';
 import Filter from './components/Filter';
 
-const mockData = [
-  { name: 'Arto Hellas', number: '040-123456', id: nanoid() },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: nanoid() },
-  { name: 'Dan Abramov', number: '12-43-234345', id: nanoid() },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: nanoid() },
-];
-
 const App = () => {
-  const personState = useState([...mockData]);
   const nameState = useState('');
   const numberState = useState('');
   const keywordState = useState('');
+  const personState = useState([]);
+  const [persons, setPersons] = personState;
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => setPersons(response.data));
+  }, [setPersons]);
 
   return (
     <div>
@@ -27,7 +27,7 @@ const App = () => {
       <Form {...{ nameState, numberState, personState }} />
 
       <h3>Numbers</h3>
-      <Persons persons={personState[0]} keyword={keywordState[0]} />
+      <Persons persons={persons} keyword={keywordState[0]} />
     </div>
   );
 };
