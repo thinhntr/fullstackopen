cebdbd0b86437d3dec78cacb69b963e7d8f1ca88
thinhntr <thinhntr@gmail.com>
2022-08-26 11:41:28 +0700

    Ex 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -40,5 +40,11 @@ app.get("/api/persons/:id", (req, res) => {
   person ? res.json(person) : res.status(404).end();
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT);
